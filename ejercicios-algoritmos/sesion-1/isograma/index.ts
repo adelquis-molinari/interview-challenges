5e83386f2a9a9043545359613d21cbc6e16b8221
adelquis-molinari <adelkis003e@gmail.com>
2023-10-10 10:46:06 -0300

    isograma

--- ejercicios-algoritmos/sesion-1/isograma/index.ts
+++ ejercicios-algoritmos/sesion-1/isograma/index.ts
@@ -1,4 +1,31 @@
+function quitarAcento(string: string) {
+  const acentos: { [key: string]: string } = {
+    á: "a",
+    é: "e",
+    í: "i",
+    ó: "o",
+    ú: "u",
+    ü: "u",
+    Á: "A",
+    É: "E",
+    Í: "I",
+    Ó: "O",
+    Ú: "U",
+    Ü: "U",
+  };
+  return string
+    .split("")
+    .map((char) => acentos[char] || char)
+    .join("");
+}
+
 export default function esIsograma(string: string): boolean {
-  // TODO: implement
-  return false;
+  const lowerCase = quitarAcento(string.toLowerCase());
+  let compared: boolean = true;
+  let charCompared: string[] = [];
+  const c = lowerCase.split("").map((char) => {
+    if (charCompared.includes(char)) return (compared = false);
+    charCompared.push(char);
+  });
+  return compared;
 }
