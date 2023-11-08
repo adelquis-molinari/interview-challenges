d43a46474fd5efbe11e6c7fac8e58214b5416af2
adelquis-molinari <adelkis003e@gmail.com>
2023-11-08 11:36:01 -0300

    ✅ letras por numeros

--- ejercicios-algoritmos/sesion-1/letras-por-numeros/index.ts
+++ ejercicios-algoritmos/sesion-1/letras-por-numeros/index.ts
@@ -1,4 +1,32 @@
+function quitarAcentos(string: string): string[] {
+  const acentos: { [key: string]: string } = {
+    á: "a",
+    é: "e",
+    í: "i",
+    ó: "o",
+    ú: "u",
+  };
+  return string
+    .split("")
+    .filter((char) => char !== " ")
+    .map((char) => acentos[char] || char);
+}
+
 export default function letrasPorNumeros(string: string): string {
-  // TODO: implement
-  return "";
+  const lowerCase: string[] = quitarAcentos(string.toLowerCase());
+
+  const ditionary: { [key: string]: number } = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+  };
+
+  const numberArray: number[] = [];
+
+  lowerCase.map((char) => numberArray.push(ditionary[char]));
+
+  return numberArray.filter((char) => char !== undefined).join(" ");
 }
