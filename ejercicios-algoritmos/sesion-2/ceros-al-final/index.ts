1acd5fe4d53bcc4ca2db46c6a256c368b3b63ca3
adelquis-molinari <adelkis003e@gmail.com>
2023-11-09 09:21:48 -0300

    start section 2 algorithm: ✅zero at the end

--- ejercicios-algoritmos/sesion-2/ceros-al-final/index.ts
+++ ejercicios-algoritmos/sesion-2/ceros-al-final/index.ts
@@ -1,4 +1,14 @@
-export default function moverCeros(array: unknown[]): unknown[] {
-  // TODO: implement
-  return [];
+export default function moverCeros(array: number[]): number[] {
+  const arrayCero: number[] = [];
+  const arrayNumber: number[] = [];
+
+  array.map((number) => {
+    if (number === 0) {
+      arrayCero.push(number);
+    } else {
+      arrayNumber.push(number);
+    }
+  });
+
+  return [...arrayNumber, ...arrayCero];
 }
