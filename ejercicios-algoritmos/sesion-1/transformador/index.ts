20af0dc9fb434602d66d49eb37455f98d4f7a457
adelquis-molinari <adelkis003e@gmail.com>
2023-11-09 08:29:02 -0300

    ✅ transformador

--- ejercicios-algoritmos/sesion-1/transformador/index.ts
+++ ejercicios-algoritmos/sesion-1/transformador/index.ts
@@ -4,12 +4,25 @@ type Input = {
 };
 
 type Output = {
-  id: string;
+  id: number;
   nombre: string;
-  edad: string;
+  edad: number;
 };
 
 export default function transformador(input: Input): Output[] {
-  // TODO: implement
-  return [];
+  const data: Output[] = [];
+
+  for (let key in input.nombres) {
+    const output: Output = {
+      id: Number(key) + 1,
+      nombre: input.nombres[key],
+      edad: input.edades[key],
+    };
+
+    data.push(output);
+  }
+
+  console.log(data);
+
+  return data;
 }
